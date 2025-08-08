63b391d434873fa6f1b3af7cdbdf581cc3949601
ArturBaghdanyan <arturbaghdanyan20@gmail.com>
2025-08-08 17:13:36 +0400

    changed filteredProducts component

---
+++ src/components/filteredProducts/index.jsx
@@ -0,0 +1,114 @@
+import { useState, useEffect } from "react";
+import { FetchData } from "../getList/fetchData";
+import SectionList from "./section";
+import menuIcon from "../../assets/menuIcon.svg";
+import close from "../../assets/close.png";
+
+import style from "./style.module.scss";
+
+const ProductsList = () => {
+  const [list, setList] = useState([]);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [error, setError] = useState("");
+  const [products, setProducts] = useState([]);
+  const [sortCriteria, setSortCriteria] = useState("");
+  const [filters, setFilters] = useState({
+    category: "",
+    brand: "",
+    priceRange: [0, 500],
+    rating: 0,
+  });
+
+  useEffect(() => {
+    const applyFilters = () => {
+      const results = products.filter((product) => {
+        const matchesCategory = filters.category
+          ? product.category === filters.category
+          : true;
+        const matchesBrand = filters.brand
+          ? product.brand === filters.brand
+          : true;
+        const matchesPrice =
+          product.price >= filters.priceRange[0] &&
+          product.price <= filters.priceRange[1];
+        const matchesRating = filters.rating
+          ? product.rating >= filters.rating
+          : true;
+        const matchesSearch = filters.search
+          ? product.name.toLowerCase().includes(filters.search.toLowerCase())
+          : true;
+        return (
+          matchesCategory &&
+          matchesBrand &&
+          matchesPrice &&
+          matchesRating &&
+          matchesSearch
+        );
+      });
+
+      if (sortCriteria) {
+        results.sort((a, b) => {
+          if (sortCriteria === "price") {
+            return a.price - b.price;
+          } else if (sortCriteria === "rating") {
+            return b.rating - a.rating;
+          } else if (sortCriteria === "popularity") {
+            return b.popularity - a.popularity;
+          }
+          return 0;
+        });
+      }
+
+      setList(results);
+    };
+
+    applyFilters();
+  }, [filters, sortCriteria]);
+
+  return (
+    <div className={style.container}>
+      <button
+        onClick={() => setMobileMenu(!mobileMenu)}
+        className={style.container_mobile_icon}
+      >
+        <img src={menuIcon} alt="mobileIcon" />
+      </button>
+
+      {mobileMenu && (
+        <div className={style.mobile_menu}>
+          <button
+            onClick={() => setMobileMenu(false)}
+            className={style.closeButton}
+          >
+            <img src={close} alt="closeButton" />
+          </button>
+
+          <SectionList
+            filters={filters}
+            setFilters={setFilters}
+            sortCriteria={sortCriteria}
+            setSortCriteria={setSortCriteria}
+            products={products}
+            setProducts={setProducts}
+            isMobile
+          />
+        </div>
+      )}
+      {error && !list.length && (
+        <div className={style.error}>do not have been found elements</div>
+      )}
+
+      <SectionList
+        filters={filters}
+        setFilters={setFilters}
+        sortCriteria={sortCriteria}
+        setSortCriteria={setSortCriteria}
+        products={products}
+        setProducts={setProducts}
+      />
+      <FetchData list={list} setList={setList} setError={setError} />
+    </div>
+  );
+};
+
+export default ProductsList;
--- src/components/getList/fetchData.js
+++ src/components/getList/fetchData.js
@@ -1,85 +1,96 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import style from "./style.module.scss";
+import { useEffect, useState, useCallback } from "react";
 import headPhoneImg from "../../assets/headphone.png";
 import speaker from "../../assets/speaker.png";
 import shoes from "../../assets/shoes.png";
 import smartphone from "../../assets/smartphone.png";
 import jacket from "../../assets/jacket.png";
 
+import style from "./style.module.scss";
 
 export const FetchData = ({ list, setList, setError }) => {
-	const [show, setShow] = useState(false);
-  
-	const dataFetch = useCallback(async () => {
-	  try {
-		const response = await fetch('/products.json');
-		if (!response.ok) {
-		  throw new Error('Failed to fetch products. Please try again later.');
-		}
-		const data = await response.json();
-  
-		const validProducts = data.products.filter(product => product.rating > 0);
-  
-		setList(validProducts);
-		localStorage.setItem('items', JSON.stringify(validProducts)); 
-		setShow(true);
-		setError('');
-	  } catch (error) {
-		setError(error.message);
-	  }
-	}, [setList, setError]);
-  
-	useEffect(() => {
-  
-	  try {
-		const items = JSON.parse(localStorage.getItem('items'));
-  
-		if (items && items.length > 0) {
-		  setList(items.filter((item) => item.rating > 0));
-		  setShow(true);
-		  setError('');
-		} else {
-		  setError('No items found in local storage!');
-		}
-	  } catch {
-		setError('Failed to access local storage.');
-	  }
-	}, [setList, setError]);
-  
-	useEffect(() => {
-	  if (list.length === 0) {
-		dataFetch();
-	  }
-	}, [list, dataFetch]);
-	
-	const imageMap = {
-		headphone: headPhoneImg,
-		speaker,
-		shoes,
-		smartphone,
-		jacket,
-	  };
+  const [show, setShow] = useState(false);
 
-return (
-	<div className={style.second}>
-		{show && (
-			<div className={style.products}>
-			{list.map((item, index) => (
-				<div key={index} className={style.products_list}>
-					<p><strong>Name:</strong> {item.name}</p>
-					<p><strong>Category:</strong> {item.category}</p>
-					<p><strong>Brand:</strong> {item.brand}</p>
-					<p><strong>Price:</strong> ${item.price}</p>
-					<p><strong>Rating:</strong> {item.rating}</p>
-					<img 
-						src={imageMap[item.imageUrl] || item.imageUrl}
-						alt={item.name} 
-						style={{ width: "100px", height: "100px" }} 
-					/>
-				</div>
-			))}
-			</div>
-		)}
-	</div>
-)
-}
+  const dataFetch = useCallback(async () => {
+    try {
+      const response = await fetch("/products.json");
+      if (!response.ok) {
+        throw new Error("Failed to fetch products. Please try again later.");
+      }
+      const data = await response.json();
+
+      const validProducts = data.products.filter(
+        (product) => product.rating > 0
+      );
+
+      setList(validProducts);
+      localStorage.setItem("items", JSON.stringify(validProducts));
+      setShow(true);
+      setError("");
+    } catch (error) {
+      setError(error.message);
+    }
+  }, [setList, setError]);
+
+  useEffect(() => {
+    try {
+      const items = JSON.parse(localStorage.getItem("items"));
+
+      if (items && items.length > 0) {
+        setList(items.filter((item) => item.rating > 0));
+        setShow(true);
+        setError("");
+      } else {
+        setError("No items found in local storage!");
+      }
+    } catch {
+      setError("Failed to access local storage.");
+    }
+  }, [setList, setError]);
+
+  useEffect(() => {
+    if (list.length === 0) {
+      dataFetch();
+    }
+  }, [list, dataFetch]);
+
+  const imageMap = {
+    headphone: headPhoneImg,
+    speaker,
+    shoes,
+    smartphone,
+    jacket,
+  };
+
+  return (
+    <div className={style.second}>
+      {show && (
+        <div className={style.products}>
+          {list.map((item, index) => (
+            <div key={index} className={style.products_list}>
+              <p>
+                <b>Name:</b> {item.name}
+              </p>
+              <p>
+                <b>Category:</b> {item.category}
+              </p>
+              <p>
+                <b>Brand:</b> {item.brand}
+              </p>
+              <p>
+                <b>Price:</b> ${item.price}
+              </p>
+              <p>
+                <b>Rating:</b> {item.rating}
+              </p>
+              <img
+                src={imageMap[item.imageUrl] || item.imageUrl}
+                alt={item.name}
+                style={{ width: "100px", height: "100px" }}
+              />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
